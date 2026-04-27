11d600954547e3d5f3d14f7fb92efb39b807a0cf
IlyaGurenko <illya4gurenko@gmail.com>
2026-04-27 21:12:46 +0300

    add controller for chat with websocket

---
+++ src/main/java/com/example/webSocket_chat/controller/ChatController.java
@@ -0,0 +1,19 @@
+package com.example.webSocket_chat.controller;
+
+import com.example.webSocket_chat.config.WebSocketConfig;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+// обрабатывает запросы
+@Controller
+public class ChatController {
+
+    // указание эндпоинта
+    @MessageMapping("/hello")
+    // разошлет всем кто подписан на данную тему
+    @SendTo("/topic/greetings")
+    public String handleMessage(String message){
+        return message;
+    }
+}
